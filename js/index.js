17fc47d0f5c2bb41491cc0dc2766102f7b2d065a
MSI\moneo <jvr.moneo@gmail.com>
2020-10-06 15:59:27 +0200

    Esquema inicial

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Crea un formulario de carrito de compra, en el que recojas mediante
+ * selectores jQuery los valores de todos los campos.
+ * 
+ * Una vez el usuario envíe el formulario, muestra en una ventana de
+ * confirmación que inicialmente no debe estar en la pantalla.
+ * 
+ * Este contendos de confirmación, debe mostrar todos los campos del
+ * formulario y sus valores en la pantalla de navegación.
+ */
